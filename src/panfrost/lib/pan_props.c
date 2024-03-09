36a0b0dd75f2f9abe24db17c45b775d959f15bba
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-03-09 08:26:09 +0800

    Update pan_props.c

--- src/panfrost/lib/pan_props.c
+++ src/panfrost/lib/pan_props.c
@@ -73,6 +73,15 @@ const struct panfrost_model panfrost_model_list[] = {
 };
 /* clang-format on */
 
+const struct panfrost_model panfrost_unknown_model = {
+   .gpu_id = 0,
+   .name = "Unknowm Mali device (Panfrost)",
+   .performance_counters = "AAAA",
+   .min_rev_anisotropic = NO_ANISO, 
+   .tilebuffer_size = 8192, 
+   .quirks = {}, 
+};
+
 #undef NO_ANISO
 #undef HAS_ANISO
 #undef MODEL
@@ -89,7 +98,7 @@ panfrost_get_model(uint32_t gpu_id)
          return &panfrost_model_list[i];
    }
 
-   return NULL;
+   return &panfrost_unknown_model;
 }
 
 /* Abstraction over the raw drm_panfrost_get_param ioctl for fetching

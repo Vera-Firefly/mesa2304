bac3b384ce07fe54ce0388074c98276d35879d22
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-03-12 01:08:03 +0800

    Update inline_sw_helper.h

--- src/gallium/auxiliary/target-helpers/inline_sw_helper.h
+++ src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -45,6 +45,10 @@
 #include "panfrost/pan_public.h"
 #endif
 
+#ifdef GALLIUM_FREEDRENO
+#include "freedreno/freedreno_public.h"
+#endif
+
 static inline struct pipe_screen *
 sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
 {
@@ -78,6 +82,18 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
       screen = d3d12_create_dxcore_screen(winsys, NULL);
 #endif
 
+#if defined(GALLIUM_FREEDRENO)
+   if(screen == NULL && strcmp(driver, "freedreno") == 0) {
+      int kbase_device_fd = open("/dev/kgsl-3d0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
+      if (kbase_device_fd == -1) { 
+         printf("FD_OSMESA: Failed to open kbase device: %s", strerror(errno));
+      } else {
+         struct pipe_screen_config dummy_cfg = { NULL, NULL };
+         screen = fd_screen_create(kbase_device_fd, &dummy_cfg, NULL);
+      }
+   }
+#endif
+
 #if defined(GALLIUM_ASAHI) && __APPLE__
    if (screen == NULL && strcmp(driver, "asahi") == 0)
       screen = agx_screen_create(0, NULL, winsys);
@@ -92,8 +108,6 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
       	screen = panfrost_create_screen(kbase_device_fd, NULL);
       }
    }
-#else
-#error You forgot to include Panfrost
 #endif
 
    return screen ? debug_screen_wrap(screen) : NULL;

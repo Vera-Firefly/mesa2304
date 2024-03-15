78c3acc50fb7b7e2e2878ce44ac5db60a9e9b30d
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-03-15 23:08:51 +0800

    Update msm_bo.c

--- src/freedreno/drm/msm/msm_bo.c
+++ src/freedreno/drm/msm/msm_bo.c
@@ -146,6 +146,7 @@ msm_bo_destroy(struct fd_bo *bo)
 
 static const struct fd_bo_funcs funcs = {
    .offset = msm_bo_offset,
+   .map = fd_bo_map_os_mmap,
    .cpu_prep = msm_bo_cpu_prep,
    .madvise = msm_bo_madvise,
    .iova = msm_bo_iova,
